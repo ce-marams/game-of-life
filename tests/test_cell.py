0ff44ed3c4b297d64300daafccb310c34d8af4a0
Carlos Elias <carloselias.mart.ams@gmail.com>
2020-11-09 00:13:12 +0100

    Create folder for the tests.

---
+++ tests/test_cell.py
@@ -0,0 +1,16 @@
+import unittest
+from unittest.mock import patch 
+from components import Cell
+
+class TestComponents(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+    
+
+if __name__ == '__main__':
+    unittest.main()
+
